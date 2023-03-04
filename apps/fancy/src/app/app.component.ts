2bb73d47192ed77746960e6cf54bfa39f3695299
qperior <konrad.nowacki@q-perior.com>
2023-03-04 17:42:10 +0100

    Initial commit

---
+++ apps/fancy/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fancy-to-do-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'fancy';
+}
