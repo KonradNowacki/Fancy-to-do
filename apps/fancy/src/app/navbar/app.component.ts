1f87cd65b861d2be02849d729e6d7225a6ba45be
qperior <konrad.nowacki@q-perior.com>
2023-03-05 10:07:02 +0100

    first commit

---
+++ apps/fancy/src/app/navbar/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <nav>Hello</nav>
+  `
+})
+export class AppComponent {
+
+}
